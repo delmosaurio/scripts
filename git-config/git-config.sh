89df6166c5b5c88111e2e83499aad30ea08c0cc2
Delmo <delmosaurio@gmail.com>
2014-04-15 16:43:07 -0300

    fix for linux

--- git-config/git-config.sh
+++ git-config/git-config.sh
@@ -1,28 +1,1 @@
-# Identity
-
-read -p "Username: " NAME
-read -p "   Email: " EMAIL
-
-git config --global user.name $NAME
-git config --global user.email $EMAIL
-
-# some shortcuts
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
-
-# Wrong writer set 1
-git config --global help.autocorrect 0
-
-# colors on the terminal
-git config --global color.ui auto
-
-# for large commits
-git config --global http.postbuffer 524288000
-
-# all logs with graph
-git config --global alias.alog "log --graph --oneline --all --decorate"
-
-# short logs
-git config --global alias.slog "log --oneline --graph --decorate"
+# Identityread -p "Username: " NAMEread -p "   Email: " EMAILgit config --global user.name $NAMEgit config --global user.email $EMAIL# some shortcutsgit config --global alias.co checkoutgit config --global alias.br branchgit config --global alias.ci commitgit config --global alias.st status# Wrong writer set 1git config --global help.autocorrect 0# colors on the terminalgit config --global color.ui auto# for large commitsgit config --global http.postbuffer 524288000# all logs with graphgit config --global alias.alog "log --graph --oneline --all --decorate"# short logsgit config --global alias.slog "log --oneline --graph --decorate"
